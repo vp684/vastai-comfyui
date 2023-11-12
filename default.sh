e56942c688f08af975bea7be87e94d5fe1c3b37e
Viraj <22423656+vp684@users.noreply.github.com>
2023-11-11 21:41:50 -0500

    Update default.sh
    revanimate comfyui

--- default.sh
+++ default.sh
@@ -3,58 +3,136 @@
 # This file will be sourced in init.sh
 
 # https://raw.githubusercontent.com/ai-dock/comfyui/main/config/provisioning/default.sh
-printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
-function download() {
-    wget -q --show-progress -e dotbytes="${3:-4M}" -O "$2" "$1"
+
+
+NODES=(
+    "https://github.com/ltdrdata/ComfyUI-Manager"
+)
+
+CHECKPOINT_MODELS=(
+        "https://civitai.com/api/download/models/129614"
+)
+
+LORA_MODELS=(
+    "https://civitai.com/api/download/models/16576"
+    "https://civitai.com/api/download/models/87153"
+    "https://civitai.com/api/download/models/62833"
+    "https://civitai.com/api/download/models/5637"
+    "https://civitai.com/api/download/models/203506"
+    "https://civitai.com/api/download/models/182689"
+    "https://civitai.com/api/download/models/208032" 
+    "https://civitai.com/api/download/models/107366"
+    "https://civitai.com/api/download/models/8339" 
+    "https://civitai.com/api/download/models/205139" 
+    "https://civitai.com/api/download/models/146600" 
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
+    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
+    "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
+)
+
+ESRGAN_MODELS=(
+    "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth"
+    "https://huggingface.co/FacehugmanIII/4x_foolhardy_Remacri/resolve/main/4x_foolhardy_Remacri.pth"
+    "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x_NMKD-Siax_200k.pth"
+)
+
+CONTROLNET_MODELS=(
+     "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors"
+    "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11f1p_sd15_depth_fp16.safetensors"    
+    "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_canny_fp16.safetensors"    
+    "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_inpaint_fp16.safetensors"    
+    "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_lineart_fp16.safetensors"    
+    "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_normalbae_fp16.safetensors"    
+    "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_openpose_fp16.safetensors"    
+    "https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_softedge_fp16.safetensors" 
+)
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    DISK_GB_AVAILABLE=$(($(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_USED=$(($(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_ALLOCATED=$(($DISK_GB_AVAILABLE + $DISK_GB_USED))
+    provisioning_print_header
+    provisioning_get_nodes
+    provisioning_get_models \
+        "/opt/ComfyUI/models/checkpoints" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/ComfyUI/models/loras" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/ComfyUI/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/ComfyUI/models/vae" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/ComfyUI/models/upscale_models" \
+        "${ESRGAN_MODELS[@]}"
+    provisioning_print_end
 }
 
-## Set paths
-nodes_dir=/opt/ComfyUI/custom_nodes
-models_dir=/opt/ComfyUI/models
-checkpoints_dir=${models_dir}/checkpoints
-vae_dir=${models_dir}/vae
-controlnet_dir=${models_dir}/controlnet
-loras_dir=${models_dir}/loras
-upscale_dir=${models_dir}/upscale_models
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="/opt/ComfyUI/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                    micromamba -n comfyui run ${PIP_INSTALL} -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}"
+            if [[ -e $requirements ]]; then
+                micromamba -n comfyui run ${PIP_INSTALL} -r "${requirements}"
+            fi
+        fi
+    done
+}
 
-## Install custom nodes
+function provisioning_get_models() {
+    if [[ -z $2 ]]; then return 1; fi
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    if [[ $DISK_GB_ALLOCATED -ge $DISK_GB_REQUIRED ]]; then
+        arr=("$@")
+    else
+        printf "WARNING: Low disk space allocation - Only the first model will be downloaded!\n"
+        arr=("$1")
+    fi
+    
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
 
-# ComfyUI-Manager
-comfyui_manager_dir=${nodes_dir}/ComfyUI-Manager
-if [[ ! -d $comfyui_manager_dir ]]; then
-    git clone https://github.com/ltdrdata/ComfyUI-Manager $comfyui_manager_dir
-else
-    (cd $comfyui_manager_dir && git pull)
-fi
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+    if [[ $DISK_GB_ALLOCATED -lt $DISK_GB_REQUIRED ]]; then
+        printf "WARNING: Your allocated disk size (%sGB) is below the recommended %sGB - Some models will not be downloaded\n" "$DISK_GB_ALLOCATED" "$DISK_GB_REQUIRED"
+    fi
+}
 
-## Download checkpoints
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Web UI will start now\n\n"
+}
 
-# v1-5-pruned-emaonly
-model_file=${checkpoints_dir}/v1-5-pruned-emaonly.ckpt
-model_url=https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt
+# Download from $1 URL to $2 file path
+function provisioning_download() {
+    wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+}
 
-if [[ ! -e ${model_file} ]]; then
-    printf "Downloading Stable Diffusion 1.5...\n"
-    download ${model_url} ${model_file}
-fi
-
-## Download controlnet
-
-model_file=${controlnet_dir}/control_canny-fp16.safetensors
-model_url=https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors
-
-if [[ ! -e ${model_file} ]]; then
-    printf "Downloading Canny...\n"
-    download ${model_url} ${model_file}
-fi
-
-## Download loras
-
-# example
-#model_file=${loras_dir}/epi_noiseoffset2.safetensors
-#model_url=https://civitai.com/api/download/models/16576
-
-#if [[ ! -e ${model_file} ]]; then
-#    printf "Downloading epi_noiseoffset2 lora...\n"
-#    download ${model_url} ${model_file}
-#fi
+provisioning_start
